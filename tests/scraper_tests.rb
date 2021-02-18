b4f0c0354b37892371798bc5522090722cbc6738
EstherH1 <47611533+EstherH1@users.noreply.github.com>
2021-02-17 22:02:23 -0500

    added test case that checks if user input returns the proper news category

---
+++ tests/scraper_tests.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require_relative '../lib/prompt'
+require_relative 
+require 'mechanize'
+
+class TestUrl < MiniTest::Test
+    #tests if user input will pull up the requested category from the method prompt
+    def test_get_type_of_stories_link
+    actual_url = get_type_of_stories_link
+        puts "please input the number 2 for testing\n"
+        expected_url = ""https://news.osu.edu/campus/"
+        assert_equal expected_url, actual_url
+    end
+end
