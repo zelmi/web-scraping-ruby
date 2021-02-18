fe0ec7acc81b298a55798d43123d0fc61caf2482
EstherH1 <hu.1808@buckeyemail.osu.edu>
2021-02-17 22:31:35 -0500

    fixed test cases to run properly

--- tests/scraper_tests.rb
+++ tests/scraper_tests.rb
@@ -6,28 +6,31 @@ require_relative '../lib/get_type_of_stories_link'
 require_relative '../lib/get_headline_link_from_user'
 
 class TestUrl < MiniTest::Test
-    #tests if user input will pull up the requested category from the method prompt
+    #tests if user input will pull up the requested category using method test_get_type_of_stories_link
     def test_get_type_of_stories_link
-    actual_url = get_type_of_stories_link
         puts "please input the number 2 for testing\n"
+    	actual_url = get_type_of_stories_link
         expected_url = "https://news.osu.edu/campus/"
         assert_equal expected_url, actual_url
     end
 
-    #tests if user input will pull up the correct article
+    #tests if user input will pull up the correct article using method get_headline_link_from_user
     def test_get_headline_link_from_user
+        # Initialize mechanize
+    	mechanize = Mechanize.new
         page = mechanize.get("https://news.osu.edu/sports/")
         # Go through each of the headings and put the link and heading into their respective arrays
         headlines = page.search('.pp_bigheadlines_heading').map {|element| Headline.new(element.text.strip, element.children[1].attributes['href'].to_s.delete_prefix('//news.osu.edu/'))}
         # Get the page with the headline the user wanted
-        puts "please input the number for testing 1 \n"
+        puts "please input the number 1 for testing \n"
         actual_url = get_headline_link_from_user(headlines)
         expected_url = "https://news.osu.edu/2021-ohio-state-big-ten-football-schedule-revision-announced/"    
         assert_equal expected_url, actual_url
     end
-        #tests to see if the method prompt recieves the correct input
+    
+    #tests to see if the method prompt recieves the correct input
     def test_prompt
-        actual_input = prompt ("please input the number 4")
+        actual_input = prompt ("please input the number 4 for testing \n")
         expected_input = "4"
         assert_equal expected_input, actual_input
     end
